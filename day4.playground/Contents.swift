3ddde6e0b7ddaad9a5fd59287047913f73b01822
Luke Reichold <lukereichold@gmail.com>
2019-12-04 16:33:49 -0600

    day 4

---
+++ day4.playground/Contents.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+// Look at every subsequent pair (R to L) for each number
+
+let monotonicallyIncreasing: [Int] = (356261 ... 846303).compactMap { num in
+    var tmp = num
+    var increasing = true
+    while tmp > 0 {
+        let lastDigit = tmp % 10
+        tmp /= 10
+        let prevDigit = tmp % 10
+
+        guard prevDigit <= lastDigit else {
+            increasing = false
+            break
+        }
+    }
+    return increasing ? num : nil
+}
+
+let results1 = monotonicallyIncreasing.filter { candidate in
+    let digits = Array(String(candidate))
+    return zip(digits, digits[1...])
+        .filter { $0.0 == $0.1 }
+        .count > 0
+}
+
+print(results1.count)
+
